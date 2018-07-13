ad4afe792052134d68848c3c4132572cb6a1f1e0
unknown <colm.scanlon@gmail.com>
2018-07-13 17:13:53 +0100

    Setup formatting for amount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
     <NavLink to={`/edit/${id}`}><h1>{description}</h1></NavLink>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+             -  
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
