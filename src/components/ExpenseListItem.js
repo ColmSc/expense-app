aefeeaf363e8fd7fdd9e57ee0555942ca0fbea13
unknown <colm.scanlon@gmail.com>
2018-07-17 16:16:54 +0100

    Updating for firebase support

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
-import selectExpensesTotal from '../selectors/expenses-total.js'
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-    <div>
-    <NavLink to={`/edit/${id}`}><h1>{description}</h1></NavLink>
-        <p>
-            {numeral(amount / 100).format('$0,0.00')}
-             -  
-            {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>
-    </div>
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
 );
 
 export default ExpenseListItem;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 
+
   const config = {
     apiKey: "AIzaSyDw5F39yP35I41QKtW8pslMQGnfx3c3kVw",
     authDomain: "expense-app-3cfd2.firebaseapp.com",
@@ -13,33 +14,132 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Test',
-    age: 222,
-    isSingle: false,
-    location: {
-        city: 'Dublin',
-        country: 'Ireland'
-    },
-    attributes: {
-        height: 1533,
-        weight: 2343
-    }
-}).then(() => {
-    console.log('Data is saved.');
-}).catch((e) => {
-    console.log('Error', e);
-});
+export { firebase, database as default };
 
-// firebase.database().ref().set('This is data');
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
-// database.ref('age').set(28);
-// database.ref('location/city').set('Galway');
-database.ref('attributes').set({
-        height: 73,
-        weight: 454
-}).then(() => {
-    console.log('Second call worked.');
-}).catch((e) => {
-    console.log('second call errored');
-})
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// }); 
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses  = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         }); 
+//         console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'Expense 1',
+//     note: 'Note 1',
+//     amount: 123,
+//     createdAt: '23423'
+// });
+
+
+// database.ref('notes').push({
+//     title: 'ToDo',
+//     body: 'Go walk'
+// });
+
+// database.ref('expenses')
+//     .on('value', (snapshot) => {
+//         const expenses  = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         }); 
+//         console.log(expenses);
+// });
+
+// const onChange = database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.name} at ${val.job.company} `)
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(55);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onChange);
+// }, 7000);
+
+// const notes = [{
+//     id: '12',
+//     title: 'First Note',
+//     body: 'This is my first note'
+// }, {
+//     id: '13',
+//     title: 'Second Note',
+//     body: 'This is my second note'
+// }]
+
+// database.ref('notes').set(notes);
+
+
+// setTimeout(() => {
+//     database.ref('age').set(90);
+// }, 10500);
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     }).catch((e) => {
+//         console.log('Error', e);
+//     })
+
+// database.ref().set({
+//     name: 'Test',
+//     age: 222,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software Engineer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Dublin',
+//         country: 'Ireland'
+//     },
+//     attributes: {
+//         height: 1533,
+//         weight: 2343
+//     }
+// }).then(() => {
+//     console.log('Data is saved.');
+// }).catch((e) => {
+//     console.log('Error', e);
+// });
+
+// database.ref().update({
+//     stressLevel: '2',
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// })
+
+// database.ref('isSingle').remove().then(function() {
+//     console.log('Removed data');
+// }).catch((e) => {
+//     console.log('Error', e);
+// });
