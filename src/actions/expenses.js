746b0fc55371e386958d6c4ded61a5427c940370
unknown <colm.scanlon@gmail.com>
2018-07-18 14:31:01 +0100

    Private db data

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -10,7 +10,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
         const {
             description = '', 
             note = '', 
@@ -19,7 +20,7 @@ export const startAddExpense = (expenseData = {}) => {
         } = expenseData
         const expense = { description, note, amount, createdAt };
 
-        database.ref('expenses').push(expense).then((ref) => {
+        database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -42,8 +43,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).update(updates).then(() => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
             dispatch(editExpense({id, updates}));
         });
     };
@@ -57,8 +59,9 @@ export const setExpenses = (expenses) => ({
 
 export const startRemoveExpense = ({id}) => {
     // const expenseId = `expenses/${id.id}`
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`)
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`)
             .remove()
             .then(() => {
                 dispatch(removeExpense({
@@ -72,7 +75,8 @@ export const startRemoveExpense = ({id}) => {
 };
 
 export const startSetExpenses = () => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
     //     const {
     //         description = '',
     //         note = '',
@@ -81,7 +85,7 @@ export const startSetExpenses = () => {
     // } = expenseData
     // const expense = { description, note, amount, createdAt };
    
-    return database.ref('expenses')
+    return database.ref(`users/${uid}/expenses`)
        .once('value')
        .then((snapshot) => {
         const expenses  = [];
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,7 +7,7 @@ import AddExpensePage from '../components/AddExpensePage';
 import HelpPage from '../components/HelpPage';
 import EditExpensePage from '../components/EditExpensePage';
 import NotFoundPage from '../components/404';
-
+import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
@@ -15,14 +15,12 @@ export const history = createHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
-         
          <Switch>
-            <Route path="/" component={LoginPage} exact={true} />
+            <PublicRoute path="/" component={LoginPage} exact={true} />
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
             <PrivateRoute path="/create" component={AddExpensePage} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
+            <PublicRoute component={NotFoundPage} />
          </Switch>
         </div>
     </Router>
