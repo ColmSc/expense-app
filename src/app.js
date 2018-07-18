590dd054ed106b151a5924f9500e27c8a0ef31e9
unknown <colm.scanlon@gmail.com>
2018-07-18 13:42:13 +0100

    Add private routes.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
+
+const jsx = (
+    <Provider store = {store}>
+        <AppRouter />
+    </Provider>
+)
+
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }  
+};
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
+});
+
+
 
 // store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000  }));
 // store.dispatch(addExpense({ description: 'Gas Bill', amount: 300, createdAt: 9992000 }));
@@ -30,16 +64,3 @@ const store = configureStore();
 //     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 //     console.log(visibleExpenses);
 // });
-
-const jsx = (
-    <Provider store = {store}>
-        <AppRouter />
-    </Provider>
-)
-
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
-
